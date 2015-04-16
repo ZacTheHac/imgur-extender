d27a5d8710fe63a1946ccb2854c8dbbf34434918
Max Azarcon <maxazarcon90@gmail.com>
2015-04-16 11:30:21 -0400

    added jquery functions

--- assets/js/style.js
+++ assets/js/style.js
@@ -7,11 +7,26 @@ $(function() {
 		settings.removeClass("hidden");
 		legal.addClass("hidden");
 		credits.addClass("hidden");
+		$("#settingsButton").addClass("selected");
+		$("#legalButton").removeClass("selected");
+		$("#creditsButton").removeClass("selected");
 	});
 	
 	$("#legalButton").on("click", function() {
 		settings.addClass("hidden");
 		legal.removeClass("hidden");
 		credits.addClass("hidden");
+		$("#settingsButton").removeClass("selected");
+		$("#legalButton").addClass("selected");
+		$("#creditsButton").removeClass("selected");
+	});
+	
+	$("#creditsButton").on("click", function() {
+		settings.addClass("hidden");
+		legal.addClass("hidden");
+		credits.removeClass("hidden");
+		$("#settingsButton").removeClass("selected");
+		$("#legalButton").removeClass("selected");
+		$("#creditsButton").addClass("selected");
 	});
 });
