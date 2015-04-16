9b63324d4c26fed2636abff3bd35609cf0cd7fff
Max Azarcon <maxazarcon90@gmail.com>
2015-04-16 11:23:06 -0400

    testing jquery

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,5 +1,17 @@
-$(function(){
-	$(this).on('click', function() {
-		if()
+$(function() {
+	var settings = $("#settingsPanel");
+	var legal = $("#legalPanel");
+	var credits = $("#creditsPanel");
+	
+	$("#settingsButton").on("click", function() {
+		settings.removeClass("hidden");
+		legal.addClass("hidden");
+		credits.addClass("hidden");
+	});
+	
+	$("#legalButton").on("click", function() {
+		settings.addClass("hidden");
+		legal.removeClass("hidden");
+		credits.addClass("hidden");
 	});
 });
